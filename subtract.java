cbaa8a78d640ec34bd06e3e94a6d31755fb95e60
Anshika <anshikabhadauria1807@gmail.com>
2025-08-07 15:08:44 +0530

    subtract.java

---
+++ subtract.java
@@ -0,0 +1,7 @@
+public class  subtract {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = b - a;
+        System.out.println("The result of subtraction is: " + result);      
+    }}
