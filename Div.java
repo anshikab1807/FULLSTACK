0f3bc39909c24bd1dc72331f1cd14bfa5114a4b4
Mahi <agrawalmahi18012006@gmail.com>
2025-08-07 15:28:54 +0530

    Div.java

---
+++ Div.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Div {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int Div = a/b;
+        System.out.println("Division : " + Div);
+    }
+    
+}
